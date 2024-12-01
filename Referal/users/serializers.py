8574f984fae095de63a7d817683768c0b6aa785c
Kryhin Maksim <masian.grodno@gmail.com>
2024-12-01 12:47:20 +0300

    add create + update serializers

--- Referal/users/serializers.py
+++ Referal/users/serializers.py
@@ -10,8 +10,20 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PhoneNumberVerificationSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(read_only=True)  # Устанавливаем поле user только для чтения
 
     class Meta:
         model = PhoneNumberVerification
-        fields = ["phone_number", "user"]
+        fields = ["phone_number", "auth_code", "referal_number", "activated_referal_number", "country_code", "user"]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user", None)  # Извлекаем данные пользователя, если они есть
+        phone_number_verification = PhoneNumberVerification.objects.create(**validated_data)
+
+        # Создаем пользователя, если данные пользователя переданы
+        if user_data:
+            user = User.objects.create(**user_data)
+            phone_number_verification.user = user
+            phone_number_verification.save()
+
+        return phone_number_verification
--- Referal/users/urls.py
+++ Referal/users/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # ----------------------------------API----------------------------------
     path("api/profile-referals/", views.ProfileRefgeralsAPIView.as_view(), name="api_profile"),
     path("api/user/", views.UserDetailAPIView.as_view(), name="api_user_detail"),
+    path("api/phone-number/create/", views.PhoneNumberVerificationCreateAPIView.as_view(), name="phone_number_create"),
+    path("api/phone-number/update/<str:phone_number>/", views.PhoneNumberVerificationUpdateAPIView.as_view(), name="phone_number_update"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
